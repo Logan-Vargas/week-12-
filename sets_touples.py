1977701211ebf185b7852afa15c10635cee2b0dd
Logan-Vargas <156100152+Logan-Vargas@users.noreply.github.com>
2025-11-18 20:05:42 +0000

    Added more

---
+++ sets_touples.py
@@ -0,0 +1,20 @@
+# #sets and touples examples:
+# #set examples
+# set1 = {1, 2, 3, 4, 5}
+# print(set1)
+# print(type(set1))
+# set1.add(6)
+# print(set1)
+# set.remove(2)
+# print(set1)
+
+set2 = {"apple", "banana", "cherry", "cherry"}
+print(set2)
+
+tuple1 = (1, 2, 3, 4, 5)
+print(tuple1)
+print(type(tuple1))
+
+
+social_security_number = (123444, 4444445, 5676789)
+print(social_security_number)
